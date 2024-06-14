ebfb334359a1fe542183101602a8188609805893
Hunsuk Choi <forestchoi@google.com>
2024-06-14 19:00:15 +0000

    Switch phones if PUK locked in DSDS configuration
    There can be cases that the Phone selected for emergency call
    is not served in the modem since the modem stack corresponding
    to the selected Phone has been stopped due to the SIM PIN/PUK
    locked state while there is another stack with an active SIM.
    
    In that case, setting emergency mode in the selected modem can
    cause abnormal behavior in the modem side.
    
    Determine whether the modem is active or not before setting
    emergency mode, and switch Phones if it's not active.
    
    Bug: 343862428
    Test: atest TelephonyConnectionServiceTest
    Manual test:
    Step 1. Power ON the device with two SIMs in 5G HOME only and
            3rd-party LTE coverage with 5G capapable SIM1 and not 5G capable SIM2.
    Step 2. Set the SIM1 preferred for calls and data.
    Step 3. Go to SIM lock settings and enter incorrect SIM PIN for SIM1, 3 times.
    Step 4. Confirm that PUK lock screen is displayed.
    Step 5. Initiate an emergency call and observe the behavior.
    
    Change-Id: I6d6edd59441e391b41dd16843aa41e6fc26ae037

--- src/com/android/services/telephony/TelephonyConnectionService.java
+++ src/com/android/services/telephony/TelephonyConnectionService.java
@@ -2672,6 +2672,12 @@ public class TelephonyConnectionService extends ConnectionService {
                     createEmergencyConnection(phone, (TelephonyConnection) resultConnection,
                             numberToDial, isTestEmergencyNumber, request, needToTurnOnRadio,
                             mEmergencyStateTracker.getEmergencyRegistrationResult());
+                } else if (result == android.telephony.DisconnectCause.EMERGENCY_PERM_FAILURE) {
+                    mEmergencyConnection.removeTelephonyConnectionListener(
+                            mEmergencyConnectionListener);
+                    TelephonyConnection c = mEmergencyConnection;
+                    releaseEmergencyCallDomainSelection(true, false);
+                    retryOutgoingOriginalConnection(c, phone, true);
                 } else {
                     mEmergencyConnection = null;
                     mAlternateEmergencyConnection = null;
@@ -3234,6 +3240,12 @@ public class TelephonyConnectionService extends ConnectionService {
                     recreateEmergencyConnection(c, phone, domain);
                     mIsEmergencyCallPending = false;
                 }, mDomainSelectionMainExecutor);
+            } else if (result == android.telephony.DisconnectCause.EMERGENCY_PERM_FAILURE) {
+                mEmergencyConnection.removeTelephonyConnectionListener(
+                        mEmergencyConnectionListener);
+                TelephonyConnection ec = mEmergencyConnection;
+                releaseEmergencyCallDomainSelection(true, false);
+                retryOutgoingOriginalConnection(ec, phone, true);
             } else {
                 mEmergencyConnection = null;
                 mAlternateEmergencyConnection = null;
