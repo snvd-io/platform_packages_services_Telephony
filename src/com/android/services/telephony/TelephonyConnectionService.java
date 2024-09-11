48672067b59bc8fdfedb70b7413f0828af16e9e1
SongFerng Wang <songferngwang@google.com>
2024-09-11 08:16:03 +0000

    Fix the intent to open SIMs settings
    Bug: 343350502
    Change-Id: Ibf111e7d1f7087cfdacbbb5ac15a64ead78b2a4f
    Test: verify pass and attach the video on the bug.
    Flag: EXEMPT bugfix

--- src/com/android/services/telephony/TelephonyConnectionService.java
+++ src/com/android/services/telephony/TelephonyConnectionService.java
@@ -4362,7 +4362,7 @@ public class TelephonyConnectionService extends ConnectionService {
                                                 context.getString(
                                                     R.string.mobile_network_settings_package),
                                                 context.getString(
-                                                    R.string.mobile_network_settings_class));
+                                                    R.string.sims_settings_class));
                                     intent.setComponent(mobileNetworkSettingsComponent);
                                     context.startActivity(intent);
                                 }
